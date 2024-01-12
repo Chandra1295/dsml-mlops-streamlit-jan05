5396fee57cedff4fa76227e837bcb9615b4a4ead
Chandra1295 <61043407+Chandra1295@users.noreply.github.com>
2024-01-12 19:18:27 +0530

    Barchat and other code is added

--- cars_24.py
+++ cars_24.py
@@ -1,68 +1,76 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import sklearn
+import datetime
 
 
 
-st.title("Car Price Prediction App")
+# df = pd.read_csv("cars24-car-price.csv")
+# st.dataframe(df)
 
-# I want to take 4 inputs from the user
-# Fuel type, Transmission type, engine power, seats
-
-
-col1, col2 = st.columns(2)
-
-with col1: 
-    # use radio for fuel_type
-    fuel_type = st.radio("Fuel Type", ["Petrol", "Diesel", "CNG", "Electric"])
-
-with col2: 
-    transmission_type = st.selectbox("Transmission Type", ["Manual", "Automatic"])
+# st.bar_chart(data=df, x= 'fuel_type', y= 'mileage')
 
 
-col3, col4 = st.columns(2)
-
-with col3:
-    engine_power = st.slider("Engine Power", 500, 5000, step=100)
-
-with col4:
-    seats = st.selectbox("Seats", [2, 4, 5, 6, 7, 8, 9, 10])
-
-
-model = pickle.load(open("car_pred", "rb"))
-
-
-# with open("car_pred", "rb") as f:
-#     model = pickle.load(f)
-
-encode_dict={
-    "fuel_type": {"Diesel": 1, "Petrol": 2, "CNG": 3, "LPG": 4, "Electric": 5},
-     "transmission": {"Manual": 1, "Automatic": 2}
-
-     }
+d = st.date_input("Enter the date", datetime.date(2023,1,2))
+st.write ("Date is:" ,d)
 
 
 
-def model_pred(fuel_type, transmission_type, engine_power, seats):
-    # create a dataframe
-    transmission_type = encode_dict["transmission"][transmission_type]
-    fuel_type = encode_dict["fuel_type"][fuel_type]
+# st.title("Car Price Prediction App")
+
+# # I want to take 4 inputs from the user
+# # Fuel type, Transmission type, engine power, seats
 
 
-    data = [[2018.0, 1, 40000, fuel_type, transmission_type, 18.0, engine_power, 85, seats]]
+# col1, col2 = st.columns(2)
 
-    return round(model.predict(data)[0], 2)
+# with col1: 
+#     # use radio for fuel_type
+#     fuel_type = st.radio("Fuel Type", ["Petrol", "Diesel", "CNG", "Electric"])
+
+# with col2: 
+#     transmission_type = st.selectbox("Transmission Type", ["Manual", "Automatic"])
 
 
-if st.button("Predict"):
-    st.write(model_pred(fuel_type, transmission_type, engine_power, seats))
-else:
-    st.write("Click on Predict, once you're done with the data")
+# col3, col4 = st.columns(2)
+
+# with col3:
+#     engine_power = st.slider("Engine Power", 500, 5000, step=100)
+
+# with col4:
+#     seats = st.selectbox("Seats", [2, 4, 5, 6, 7, 8, 9, 10])
+
+
+# model = pickle.load(open("car_pred", "rb"))
 
 
 
 
-# pip freeze > requirements.txt
-# pipreqs .
-    
+# # with open("car_pred", "rb") as f:
+# #     model = pickle.load(f)
 
+# encode_dict={
+#     "fuel_type": {"Diesel": 1, "Petrol": 2, "CNG": 3, "LPG": 4, "Electric": 5},
+#      "transmission": {"Manual": 1, "Automatic": 2}
+
+#      }
+
+
+
+# def model_pred(fuel_type, transmission_type, engine_power, seats):
+#     # create a dataframe
+#     transmission_type = encode_dict["transmission"][transmission_type]
+#     fuel_type = encode_dict["fuel_type"][fuel_type]
+
+
+#     data = [[2018.0, 1, 40000, fuel_type, transmission_type, 18.0, engine_power, 85, seats]]
+
+#     return round(model.predict(data)[0],2)
+
+
+
+# if st.button("Predict"):
+#     st.write(model_pred(fuel_type, transmission_type, engine_power, seats))
+# else:
+#     st.write("Click on Predict, once you're done with the data")
